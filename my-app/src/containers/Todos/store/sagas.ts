edb55146d554e8ad76a75d558e20483eab8428c9
Evhen-y <67057105+Evhen-y@users.noreply.github.com>
2022-02-11 11:03:30 +0300

    add-saga

--- my-app/src/containers/Todos/store/sagas.ts
+++ my-app/src/containers/Todos/store/sagas.ts
@@ -1,1 +1,100 @@
-export {};
+import { takeLatest, put, call } from "redux-saga/effects";
+import { todosActions, TodosActionsTypes } from ".";
+
+function* fetchTodosSaga({ payload, cb }: ReturnType<any>) {
+  try {
+    const todos = [
+      {
+        id: 1,
+        text: "Todods 001",
+        createAt: new Date(),
+        completed: false,
+      },
+      {
+        id: 2,
+        text: "Todods 002",
+        createAt: new Date(),
+        completed: false,
+      },
+    ];
+
+    yield put(todosActions.FETCH_TODOS.SUCCESS(todos));
+  } catch (err) {
+    yield put(todosActions.FETCH_TODOS.FAILURE(err as Object));
+  } finally {
+    cb?.();
+  }
+}
+function* fetchTodoSaga({ payload, cb }: ReturnType<any>) {
+  try {
+    const todo = [
+      {
+        id: 3,
+        text: "Todods 003",
+        createAt: new Date(),
+        completed: false,
+      },
+    ];
+
+    yield put(todosActions.FETCH_TODO.SUCCESS(todo));
+  } catch (err) {
+    yield put(todosActions.FETCH_TODO.FAILURE(err as Object));
+  } finally {
+    cb?.();
+  }
+}
+
+function* addTodoSaga({ payload, cb }: ReturnType<any>) {
+  try {
+    const newTodo = [
+      {
+        id: 4,
+        text: "Todods 004",
+        createAt: new Date(),
+        completed: false,
+      },
+    ];
+    yield put(todosActions.ADD_TODO.SUCCESS(newTodo));
+  } catch (err) {
+    yield put(todosActions.ADD_TODO.FAILURE(err as Object));
+  } finally {
+    cb?.();
+  }
+}
+
+function* editTodoSaga({ payload, cb }: ReturnType<any>) {
+  try {
+    const updateTodo = [
+      {
+        id: 44,
+        text: "Todods 0044",
+        createAt: new Date(),
+        completed: false,
+      },
+    ];
+
+    yield put(todosActions.EDIT_TODO.SUCCESS(updateTodo));
+  } catch (err) {
+    yield put(todosActions.ADD_TODO.FAILURE(err as Object));
+  } finally {
+    cb?.();
+  }
+}
+
+function* removeTodoSaga({ payload, cb }: ReturnType<any>) {
+  try {
+    yield put(todosActions.REMOVE_TODO.SUCCESS(payload));
+  } catch (err) {
+    yield put(todosActions.REMOVE_TODO.FAILURE(err as Object));
+  } finally {
+    cb?.();
+  }
+}
+
+export const todosWatcherSaga = function* () {
+  yield takeLatest(TodosActionsTypes.FETCH_TODOS.REQUEST, fetchTodosSaga);
+  yield takeLatest(TodosActionsTypes.FETCH_TODO.REQUEST, fetchTodoSaga);
+  yield takeLatest(TodosActionsTypes.ADD_TODO.REQUEST, addTodoSaga);
+  yield takeLatest(TodosActionsTypes.EDIT_TODO.REQUEST, editTodoSaga);
+  yield takeLatest(TodosActionsTypes.REMOVE_TODO.REQUEST, removeTodoSaga);
+};
--- my-app/src/utils/actionsCreator.ts
+++ my-app/src/utils/actionsCreator.ts
@@ -1,7 +1,8 @@
 import { TYPES } from "./actionsTypeCreator";
+import { AnyAction } from "redux";
 
 interface IActions {
-  [key: string]: { [key: string]: (payload: Object, options?: Object, cb?: () => void) => Object };
+  [key: string]: { [key: string]: (payload: Object, options?: Object, cb?: () => void) => AnyAction };
 }
 
 export const actionsCreator = (aType: string[]): IActions => {
@@ -12,8 +13,8 @@ export const actionsCreator = (aType: string[]): IActions => {
       response[at][t] = (payload: Object, options?: Object, cb?: () => void) => ({
         type: `${at}_${t}`,
         payload,
-        options,
         cb,
+        options,
       });
     });
   });
