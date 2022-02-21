3e59d8556987824b314e348e6c78b4d6394b4616
Evhen-y <67057105+Evhen-y@users.noreply.github.com>
2022-02-21 16:21:29 +0300

    finish redux"

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,6 +1,15 @@
-import React from "react";
-import { TodoContainers } from "@containers/";
+import React, {useEffect} from "react";
+import {useDispatch} from "react-redux";
+import { TodoContainers, todosActions } from "@containers/";
 
-const App = () => <TodoContainers />;
 
+
+const App = () => {
+    const dispatch = useDispatch()
+useEffect(()=>{
+    dispatch(todosActions.FETCH_TODOS.REQUEST({}))
+}, [])
+return<TodoContainers />;
+
+}
 export default App;
--- my-app/src/containers/Todos/components/Todos/Todos.tsx
+++ my-app/src/containers/Todos/components/Todos/Todos.tsx
@@ -1,16 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import { ITodo } from "../../store/interface";
-
+import {todosActions, getTodos} from "@containers/"
+import {useSelector} from "react-redux"
 const Todos = () => {
-  const [todos, setTodos] = useState<ITodo[] | undefined>([
-    {
-      id: 1,
-      text: "Todo 001",
-      createAt: new Date(),
-      complited: false,
-    },
-  ]);
-
+  // const [todos, setTodos] = useState<ITodo[] | undefined>([
+  //   {
+  //     id: 1,
+  //     text: "Todo 001",
+  //     createAt: new Date(),
+  //     complited: false,
+  //   },
+  // ]);
+  const todos = useSelector(getTodos())
   return (
     <>
       {todos?.map(({ text, id }) => (
