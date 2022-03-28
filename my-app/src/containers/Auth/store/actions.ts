c9165ca9d2a8c58b8d03984f28b5a83cb9d280a6
Evhen-y <67057105+Evhen-y@users.noreply.github.com>
2022-03-28 23:01:56 +0200

    add Authreduser

---
+++ my-app/src/containers/Auth/store/actions.ts
@@ -0,0 +1,4 @@
+import {actionsCreator} from "@utils/"
+import {ACTIONS_TYPES_AUTH} from "./constans"
+
+export const AuthActions = actionsCreator(ACTIONS_TYPES_AUTH)
---
+++ my-app/src/containers/Auth/store/interface.ts
@@ -0,0 +1,9 @@
+import {IUser} from "@containers/"
+
+export interface IAuthState {
+    error: null | string;
+    isAuthentificate: boolean;
+    authUser : null | Partial<IUser> | number;
+    token: null | string;
+    loading: boolean;
+}
