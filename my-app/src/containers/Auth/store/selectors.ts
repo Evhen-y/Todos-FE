3b667b3ff078735382ddbfba4bebc66a28dd4f50
Evhen-y <67057105+Evhen-y@users.noreply.github.com>
2022-04-02 07:04:43 +0200

    ok

--- my-app/src/containers/Auth/store/selectors.ts
+++ my-app/src/containers/Auth/store/selectors.ts
@@ -3,5 +3,5 @@ import { IAppState } from "@shared/";
 
 const selectAuth = (state: IAppState) => state.AuthReduser;
 
-// export const getUser = () => createSelector(selectAuth, (state) => state.authUser);
+export const getUserAuth = () => createSelector(selectAuth, (state) => state.authUser);
 export const getIsAuthentificate = () => createSelector(selectAuth, (state) => state.isAuthentificate);
